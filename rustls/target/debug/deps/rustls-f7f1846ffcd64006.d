042335cea3ea97d0382fa95a5db029cd0828a99c
Ted Lee <118622427+Cpoing@users.noreply.github.com>
2025-11-06 00:39:40 -0500

    Delete rustls/target directory

--- rustls/target/debug/deps/rustls-f7f1846ffcd64006.d
+++
@@ -1,5 +0,0 @@
-/Users/tedlee/Documents/rust-ls/rustls/target/debug/deps/rustls-f7f1846ffcd64006: src/main.rs
-
-/Users/tedlee/Documents/rust-ls/rustls/target/debug/deps/rustls-f7f1846ffcd64006.d: src/main.rs
-
-src/main.rs:
--- rustls/target/debug/rustls.d
+++
@@ -1,1 +0,0 @@
-/Users/tedlee/Documents/rust-ls/rustls/target/debug/rustls: /Users/tedlee/Documents/rust-ls/rustls/src/main.rs
